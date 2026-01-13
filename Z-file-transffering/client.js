46394caa22c772da57b14102bb3b2df70b639fa0
Daneshwar <saiguptha663@gmail.com>
2026-01-13 05:28:25 +0000

    changes on file transfer in two server to client

--- Z-file-transffering/client.js
+++ Z-file-transffering/client.js
@@ -2,52 +2,33 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const fs = require("fs");
 const path = require("path");
-const PROTO_PATH = path.join(__dirname,"./proto/file_transfer.proto");
 
+const PROTO_PATH = path.join(__dirname, "proto/file_transfer.proto");
 const SERVER_ADDRESS = "0.0.0.0:50051";
 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
-
-const proto = grpc.loadPackageDefinition(packageDefinition)
-  .filetransfer;
+const packageDef = protoLoader.loadSync(PROTO_PATH);
+const proto = grpc.loadPackageDefinition(packageDef).filetransfer;
 
 function downloadFile(fileName, outputFile) {
-  let offset = 0;
-
-  if (fs.existsSync(outputFile)) {
-    offset = fs.statSync(outputFile).size;
-    console.log(`Resuming download from byte ${offset}`);
-  }
+  let offset = fs.existsSync(outputFile)
+    ? fs.statSync(outputFile).size
+    : 0;
 
   const client = new proto.FileTransferService(
     SERVER_ADDRESS,
     grpc.credentials.createInsecure()
   );
 
-  const call = client.DownloadFile({
-    file_name: fileName,
-    offset: offset,
-  });
-
+  const call = client.DownloadFile({ file_name: fileName, offset });
   const writeStream = fs.createWriteStream(outputFile, { flags: "a" });
 
   call.on("data", (chunk) => {
     if (chunk.eof) {
-      console.log("Download completed");
       writeStream.close();
+      console.log("Download completed");
       return;
     }
     writeStream.write(chunk.data);
-  });
-
-  call.on("end", () => {
-    console.log("Stream ended");
   });
 
   call.on("error", (err) => {
@@ -56,4 +37,4 @@ function downloadFile(fileName, outputFile) {
   });
 }
 
-downloadFile("sample.txt", "downloaded_sample.txt");
+downloadFile("practice.java", "downloaded_practice.java");
--- Z-file-transffering/server.js
+++ Z-file-transffering/server.js
@@ -3,26 +3,17 @@ const protoLoader = require("@grpc/proto-loader");
 const fs = require("fs");
 const path = require("path");
 
-const PROTO_PATH = path.join(__dirname,"./proto/file_transfer.proto")
-const CHUNK_SIZE = 64 * 1024; // 64KB
+const PROTO_PATH = path.join(__dirname, "proto/file_transfer.proto");
+const FILE_PATH = "C:/Users/K Daneshwar guptha/OneDrive/Desktop/JavaScript/Day2/practice.java";
+const CHUNK_SIZE = 64 * 1024;
 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
-
-const proto = grpc.loadPackageDefinition(packageDefinition)
-  .filetransfer;
+const packageDef = protoLoader.loadSync(PROTO_PATH);
+const proto = grpc.loadPackageDefinition(packageDef).filetransfer;
 
 function DownloadFile(call) {
-  const fileName = call.request.file_name;
   const offset = Number(call.request.offset);
-  const filePath = "C:/Users/K Daneshwar guptha/OneDrive/Desktop/JavaScript/Day2/practice.java";
 
-  if (!fs.existsSync(filePath)) {
+  if (!fs.existsSync(FILE_PATH)) {
     call.emit("error", {
       code: grpc.status.NOT_FOUND,
       message: "File not found",
@@ -30,16 +21,14 @@ function DownloadFile(call) {
     return;
   }
 
-  const fileSize = fs.statSync(filePath).size;
   let chunkNumber = Math.floor(offset / CHUNK_SIZE);
 
-  const stream = fs.createReadStream(filePath, {
+  const stream = fs.createReadStream(FILE_PATH, {
     start: offset,
     highWaterMark: CHUNK_SIZE,
   });
 
   stream.on("data", (chunk) => {
-    console.log("client connected");
     call.write({
       data: chunk,
       chunk_number: chunkNumber++,
@@ -64,21 +53,14 @@ function DownloadFile(call) {
   });
 }
 
-function main() {
-  const server = new grpc.Server();
+const server = new grpc.Server();
+server.addService(proto.FileTransferService.service, { DownloadFile });
 
-  server.addService(proto.FileTransferService.service, {
-    DownloadFile,
-  });
-
-  server.bindAsync(
-    "0.0.0.0:50051",
-    grpc.ServerCredentials.createInsecure(),
-    () => {
-      console.log("Server running on port 50051");
-     
-    }
-  );
-}
-
-main();
+server.bindAsync(
+  "0.0.0.0:50051",
+  grpc.ServerCredentials.createInsecure(),
+  () => {
+    server.start();
+    console.log("Server running on port 50051");
+  }
+);
